97cf1a27414c4c93087d6a0f0d9ab5ed1f6d025b
Choi-jujuyeon <chlwndus1216@naver.com>
2023-06-07 00:02:03 +0900

    계산기만들기-2-상태관리 변수설정

--- 9-calculator/src/components/Calculator.jsx
+++ 9-calculator/src/components/Calculator.jsx
@@ -1,7 +1,18 @@
-import { Box } from '@chakra-ui/react';
-
+import { Box, Flex, Input } from '@chakra-ui/react';
+import { useState } from 'react';
 const Calculator = () => {
-  return <Box>Calculator</Box>;
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState();
+
+  return (
+    <Box>
+      <Flex>
+        <Input />
+        <Input ml={2} />
+      </Flex>
+    </Box>
+  );
 };
 
 export default Calculator;
