6e841757706b981a04787e18efc1e3d932680b7e
Choi-jujuyeon <chlwndus1216@naver.com>
2023-06-06 22:35:38 +0900

    주간계획표 만들기-2-props+map()

---
+++ 6-weekly-planner/src/components/Todo.jsx
@@ -0,0 +1,25 @@
+import { Box, Flex } from '@chakra-ui/react';
+
+const Todo = props => {
+  return (
+    <Box w={200} h={200} bgColor="white" mx={4} rounded="3xl">
+      <Flex
+        bgColor={props.bgColor}
+        h="20%"
+        roundedTop="3xl"
+        alignItems="center"
+        pl={4}
+      >
+        {props.day}
+      </Flex>
+
+      <Flex direction="column" p={4}>
+        {props.todos.map((v, i) => {
+          return <Box key={i}>{v}</Box>;
+        })}
+      </Flex>
+    </Box>
+  );
+};
+
+export default Todo;
