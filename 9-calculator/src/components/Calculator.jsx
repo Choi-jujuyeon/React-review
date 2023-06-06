0fdaa53a88e62815c85f5c117db4788a9acf4767
Choi-jujuyeon <chlwndus1216@naver.com>
2023-06-07 00:51:52 +0900

    계산기만들기-6-연산기능 넣어 완성(onclick-useState)

--- 9-calculator/src/components/Calculator.jsx
+++ 9-calculator/src/components/Calculator.jsx
@@ -12,25 +12,60 @@ const Calculator = () => {
   };
 
   const onChangeNum2 = e => {
-    setNum1(e.target.value);
+    setNum2(e.target.value);
+  };
+
+  const onClickSum = () => {
+    //문자 값이 들어가지 않도록 설정하기
+    if (isNaN(num1) || isNaN(num2)) {
+      return;
+    }
+    //각 input값은 문자열로 들어오기 때문에 형변환 과정이 필요하다.
+    setResult(parseInt(num1) + parseInt(num2));
+  };
+
+  //빼기
+  const onClickSubtract = () => {
+    if (isNaN(num1) || isNaN(num2)) {
+      return;
+    }
+    setResult(parseInt(num1) - parseInt(num2));
+  };
+
+  //곱하기
+  const onClickMultiply = () => {
+    if (isNaN(num1) || isNaN(num2)) {
+      return;
+    }
+    setResult(parseInt(num1) * parseInt(num2));
+  };
+
+  //나누기
+  const onClickDivide = () => {
+    if (isNaN(num1) || isNaN(num2)) {
+      return;
+    }
+    setResult(parseInt(num1) / parseInt(num2));
   };
 
   return (
     <Box>
-      <Flex>
+      <Flex alignItems="center">
         <Input value={num1} onChange={onChangeNum1} />
         <Input value={num2} onChange={onChangeNum2} ml={2} />
         {result && <Box ml={4}>{result}</Box>}
       </Flex>
       <Box mt={4}>
-        <Button colorScheme="red">더하기</Button>
-        <Button ml={4} colorScheme="blue">
+        <Button colorScheme="red" onClick={onClickSum}>
+          더하기
+        </Button>
+        <Button ml={4} colorScheme="blue" onClick={onClickSubtract}>
           빼기
         </Button>
-        <Button ml={4} colorScheme="green">
+        <Button ml={4} colorScheme="green" onClick={onClickMultiply}>
           곱하기
         </Button>
-        <Button ml={4} colorScheme="blue">
+        <Button ml={4} colorScheme="blue" onClick={onClickDivide}>
           나누기
         </Button>
       </Box>
