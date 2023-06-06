fd44c615479ddfd4fcf2176767dea5112f73244b
Choi-jujuyeon <chlwndus1216@naver.com>
2023-06-06 17:44:01 +0900

    3-Chakra UI-기본세팅

---
+++ 3-chackra-ui/src/App.jsx
@@ -0,0 +1,11 @@
+import { ChakraProvider, Box } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Box>Hello REACT~~!~</Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
