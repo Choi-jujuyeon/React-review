90c7730bf9a98011f62523a87340fa2304b87b40
Choi-jujuyeon <chlwndus1216@naver.com>
2023-06-06 22:49:59 +0900

    useState-버튼 클릭해 초기값 업데이트해주기

--- 7-use-state/src/App.jsx
+++ 7-use-state/src/App.jsx
@@ -1,9 +1,14 @@
-import { ChakraProvider, Flex } from '@chakra-ui/react';
+import { ChakraProvider, Flex, Button } from '@chakra-ui/react';
 import { useState } from 'react';
 
 function App() {
   //const [호출할 변수명, 업데이트할 변수명] = useState(초기값 넣어주기);
   const [name, setName] = useState('ABC');
+
+  const onClickUpdate = () => {
+    //setName(업데이트할 값을 넣어준다);
+    setName('DEF');
+  };
 
   return (
     <ChakraProvider>
@@ -14,6 +19,9 @@ function App() {
         alignItems="center"
       >
         {name}
+        <Button ml={4} onClick={onClickUpdate}>
+          Update
+        </Button>
       </Flex>
     </ChakraProvider>
   );
