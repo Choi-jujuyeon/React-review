b8f5bb47532ec3510ea5501fa2b681b175a27223
Choi-jujuyeon <chlwndus1216@naver.com>
2023-06-06 22:48:00 +0900

    useState-초기값

--- 7-use-state/src/App.jsx
+++ 7-use-state/src/App.jsx
@@ -1,6 +1,10 @@
 import { ChakraProvider, Flex } from '@chakra-ui/react';
+import { useState } from 'react';
 
 function App() {
+  //const [호출할 변수명, 업데이트할 변수명] = useState(초기값 넣어주기);
+  const [name, setName] = useState('ABC');
+
   return (
     <ChakraProvider>
       <Flex
@@ -9,7 +13,7 @@ function App() {
         justifyContent="center"
         alignItems="center"
       >
-        Hello REACT~!~
+        {name}
       </Flex>
     </ChakraProvider>
   );
