e7b5a763a1de8c095e237c8610496140ec11b1e8
Choi-jujuyeon <chlwndus1216@naver.com>
2023-06-06 17:55:18 +0900

    Box-ui 5개 만들기

--- 3-chackra-ui/src/App.jsx
+++ 3-chackra-ui/src/App.jsx
@@ -9,10 +9,23 @@ function App() {
         justifyContent="center"
         alignItems="center"
       >
-        <Box>Hello REACT~~!~</Box>
-        <Button ml={4} colorScheme="purple">
-          버튼
-        </Button>
+        {/* Box태그 == div 태그와 유사한 기능 */}
+
+        <Box w={200} h={200} bgColor="yellow.100">
+          Box1
+        </Box>
+        <Box w={200} h={200} bgColor="yellow.200">
+          Box2
+        </Box>
+        <Box w={200} h={200} bgColor="yellow.300">
+          Box3
+        </Box>
+        <Box w={200} h={200} bgColor="yellow.400">
+          Box4
+        </Box>
+        <Box w={200} h={200} bgColor="yellow.500">
+          Box5
+        </Box>
       </Flex>
     </ChakraProvider>
   );
