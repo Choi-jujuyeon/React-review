02004f0ebd334859fc7a20e58cc0a5a3331bbacd
Choi-jujuyeon <chlwndus1216@naver.com>
2023-06-06 17:49:47 +0900

    가운데 정렬 + 스타일적용

--- 3-chackra-ui/src/App.jsx
+++ 3-chackra-ui/src/App.jsx
@@ -3,9 +3,16 @@ import { Box, Button, Flex, ChakraProvider } from '@chakra-ui/react';
 function App() {
   return (
     <ChakraProvider>
-      <Flex>
+      <Flex
+        bgColor="gray.100"
+        minH="100vh"
+        justifyContent="center"
+        alignItems="center"
+      >
         <Box>Hello REACT~~!~</Box>
-        <Button>버튼</Button>
+        <Button ml={4} colorScheme="purple">
+          버튼
+        </Button>
       </Flex>
     </ChakraProvider>
   );
